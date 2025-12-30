9298e0373f2ff024f777d216115c8deba736796f
agusoyo <132808184+agusoyo@users.noreply.github.com>
2025-12-30 21:55:46 +0100

    Update Gallery.tsx

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -4,6 +4,11 @@ import React, { useState, useEffect, useCallback } from 'react';
 const GITHUB_BASE_URL = "https://raw.githubusercontent.com/agusoyo/Dobao/main/";
 
 const IMAGES = [
+   {
+    fileName: "Escaparate Dobao.jpg", 
+    title: "Escaparate DAOBAO",
+    desc: "Nuestro escaparate es un Reflejo del Arte que llevamos dentro"
+  },
   {
     fileName: "IMG_4292.jpeg", 
     title: "La Mesa Imperial",
