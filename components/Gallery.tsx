a7754815eac873332668c31987516782fc363169
agusoyo <132808184+agusoyo@users.noreply.github.com>
2026-01-11 12:46:35 +0100

    Update Gallery.tsx

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -5,6 +5,11 @@ const GITHUB_BASE_URL = "https://raw.githubusercontent.com/agusoyo/Dobao/main/";
 
 const IMAGES = [
   {
+    fileName: "Escaparate Dobao.jpg", 
+    title: "Nuestro Escaparate",
+    desc: "El ARTE en todos los sentidos es parte de nuestro ADN."
+  }
+  {
     fileName: "IMG_4292.jpeg", 
     title: "La Mesa Imperial",
     desc: "Iluminación de diseño y madera noble para banquetes inolvidables."
